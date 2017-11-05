105c54549b0772494afc0f9ac05174f47953b09d
Ralph <ralph_young@163.com>
2017-11-05 12:21:21 +0800

    newer

--- B5.py
+++ B5.py
@@ -13,11 +13,23 @@ def hello_world():
 def botUI():
     return render_template('botUI.html')
 
-@app.route('/getData')
+@app.route('/getBookData')
 def getBookData():
     t = person.data()
     print(t)
-    return json.dumps(t['bestBook'],ensure_ascii=False)
+    return json.dumps(t['bestBook']+"==========="+t['bestBookComment'],ensure_ascii=False)
+
+@app.route('/getMusicData')
+def getMusicData():
+    t = person.data()
+    print(t)
+    return json.dumps(t['bestMusic']+"==========="+t['bestMusicComment'],ensure_ascii=False)
+
+@app.route('/getMovieData')
+def getMovieData():
+    t = person.data()
+    print(t)
+    return json.dumps(t['bestMovie']+"==========="+t['bestMovieComment'],ensure_ascii=False)
 
 if __name__ == '__main__':
     app.debug = True
